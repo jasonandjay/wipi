ce586a5cbcd1499de827257a4183e07afa1e262b
jasonandjay <342690199@qq.com>
2021-08-12 19:11:23 +0800

    updtae notify

--- packages/server/src/modules/alipay/alipay.module.ts
+++ packages/server/src/modules/alipay/alipay.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { ArticleModule } from '../article/article.module';
 import { AlipayController } from './alipay.controller';
 import { TagModule } from '../tag/tag.module';
 import { CategoryModule } from '../category/category.module';
-
-const passModule = PassportModule.register({ defaultStrategy: 'jwt' });
-const jwtModule = JwtModule.register({
-  secret: 'wipi',
-  signOptions: { expiresIn: '4h' },
-});
 
 @Module({
   imports: [CategoryModule, TagModule, ArticleModule],
