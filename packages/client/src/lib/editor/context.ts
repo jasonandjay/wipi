cdf2c1c973ab4285ac7f438b242198c5984fb85e
jasonandjay <342690199@qq.com>
2021-08-17 12:18:03 +0800

    remove editor

--- packages/client/src/lib/editor/context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-type Props = {
-	lang: string;
-};
-
-export default createContext<Props>({
-	lang: 'en-US',
-});
--- packages/client/src/lib/editor/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-import useDispatch from './use-dispatch';
-import useSelector from './use-selector';
-
-export { useDispatch, useSelector };
